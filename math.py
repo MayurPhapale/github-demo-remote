30216e05564dc3e60bd72e857c5cc8b26a66024a
Mayur Phapale <mayurphapale44@gmail.com>
2022-01-31 20:22:22 +0530

    Added function header to math.py

--- math.py
+++ math.py
@@ -1,15 +1,15 @@
-
+#addition Function
 def add(a,b):
 	pass
     
-
+#substraction function
 def sub(a,b):
 	pass
     
- 
+ #multiplication function
 def multi(a,b):
 	pass
     
-
+#divide function
 def div(a,b):   
 	pass
